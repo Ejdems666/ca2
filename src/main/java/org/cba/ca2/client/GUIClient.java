737687a33a3dfdb38955a96344ad164bd3cdfc32
Adam Becvar <becvar.adam666@gmail.com>
2017-10-03 10:23:02 +0200

    Merge Petrus GUI changes and Remove error messages

--- src/main/java/org/cba/ca2/client/GUIClient.java
+++ src/main/java/org/cba/ca2/client/GUIClient.java
@@ -11,15 +11,18 @@ import java.util.Scanner;
 
 public class GUIClient {
 
+    String      appName     = "SOCKET CHAT 0.1V";
     JFrame newFrame = new JFrame("Colt Chat v0.1");
     JButton sendMessage;
     JTextField messageBox;
     JTextArea chatBox;
     JTextField usernameChooser;
     JFrame preFrame;
+    JTextField  ipAddress;
+    JTextField  port;
     private Socket serverSocket;
     private PrintWriter serverOutput;
-    private final Scanner serverInput;
+    private Scanner serverInput;
 
     public static void main(String[] args) throws IOException {
         try {
@@ -31,68 +34,87 @@ public class GUIClient {
         GUIClient.renderLoginFrame();
     }
 
-    public GUIClient() throws IOException {
-        serverSocket = new Socket("localhost", 1235);
-        serverOutput = new PrintWriter(serverSocket.getOutputStream(), true);
-        serverInput = new Scanner(serverSocket.getInputStream());
-    }
-
     public void renderLoginFrame() {
         newFrame.setVisible(false);
-        preFrame = new JFrame("Choose your username!(Colt chat v0.1");
-        usernameChooser = new JTextField();
+        preFrame = new JFrame(appName);
+        usernameChooser = new JTextField(25);
+        ipAddress = new JTextField(25);
+        port = new JTextField(25);
         JLabel chooseUsernameLabel = new JLabel("Pick a username:");
+        JLabel chooseIpAddress = new JLabel("Insert IP address:");
+        JLabel choosePort = new JLabel("Insert Port");
         JButton enterServer = new JButton("Enter Chat Server");
         JPanel prePanel = new JPanel(new GridBagLayout());
 
         GridBagConstraints preRight = new GridBagConstraints();
+        preRight.insets = new Insets(0, 0, 0, 10);
         preRight.anchor = GridBagConstraints.EAST;
         GridBagConstraints preLeft = new GridBagConstraints();
         preLeft.anchor = GridBagConstraints.WEST;
-        preRight.weightx = 2.0;
+        preLeft.insets = new Insets(0, 10, 0, 10);
+        // preRight.weightx = 2.0;
         preRight.fill = GridBagConstraints.HORIZONTAL;
         preRight.gridwidth = GridBagConstraints.REMAINDER;
 
         prePanel.add(chooseUsernameLabel, preLeft);
         prePanel.add(usernameChooser, preRight);
+        prePanel.add(chooseIpAddress, preLeft);
+        prePanel.add(ipAddress, preRight);
+        prePanel.add(choosePort, preLeft);
+        prePanel.add(port, preRight);
         preFrame.add(BorderLayout.CENTER, prePanel);
         preFrame.add(BorderLayout.SOUTH, enterServer);
+        preFrame.setSize(600, 600);
         preFrame.setVisible(true);
-        preFrame.setSize(300, 300);
         preFrame.getRootPane().setDefaultButton(enterServer);
 
-        enterServer.addActionListener(new enterServerButtonListener(serverOutput));
+        enterServer.addActionListener(new enterServerButtonListener());
     }
 
     public void renderChatFrame() {
-        newFrame.setVisible(true);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
         JPanel southPanel = new JPanel();
-        newFrame.add(BorderLayout.SOUTH, southPanel);
         southPanel.setBackground(Color.BLUE);
         southPanel.setLayout(new GridBagLayout());
 
-        messageBox = new JTextField(30);
+        messageBox = new JTextField(40);
+        messageBox.requestFocusInWindow();
+
         sendMessage = new JButton("Send Message");
+        sendMessage.addActionListener(new sendMessageButtonListener());
+
         chatBox = new JTextArea();
         chatBox.setEditable(false);
-        newFrame.add(new JScrollPane(chatBox), BorderLayout.CENTER);
-
+        chatBox.setFont(new Font("Serif", Font.PLAIN, 25));
         chatBox.setLineWrap(true);
+        printLineToChatBox("server","You're logged in as"+username);
+
+        mainPanel.add(new JScrollPane(chatBox), BorderLayout.CENTER);
 
         GridBagConstraints left = new GridBagConstraints();
-        left.anchor = GridBagConstraints.WEST;
+        left.anchor = GridBagConstraints.LINE_START;
+        left.fill = GridBagConstraints.HORIZONTAL;
+        left.weightx = 512.0D;
+        left.weighty = 1.0D;
+
         GridBagConstraints right = new GridBagConstraints();
-        right.anchor = GridBagConstraints.EAST;
-        right.weightx = 2.0;
+        right.insets = new Insets(0, 10, 0, 0);
+        right.anchor = GridBagConstraints.LINE_END;
+        right.fill = GridBagConstraints.NONE;
+        right.weightx = 1.0D;
+        right.weighty = 1.0D;
 
         southPanel.add(messageBox, left);
         southPanel.add(sendMessage, right);
 
-        chatBox.setFont(new Font("Serif", Font.PLAIN, 15));
-        chatBox.append("You're logged in as "+username+"\n");
-        sendMessage.addActionListener(new sendMessageButtonListener(serverOutput));
+        mainPanel.add(BorderLayout.SOUTH, southPanel);
+
+        newFrame.add(mainPanel);
         newFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        newFrame.setSize(470, 300);
+        newFrame.setSize(870, 500);
+        newFrame.setVisible(true);
         newFrame.getRootPane().setDefaultButton(sendMessage);
 
         new Thread(() -> {
@@ -114,12 +136,6 @@ public class GUIClient {
     }
 
     class sendMessageButtonListener implements ActionListener {
-        private final PrintWriter serverOutput;
-
-        sendMessageButtonListener(PrintWriter serverOutput) {
-            this.serverOutput = serverOutput;
-        }
-
         public void actionPerformed(ActionEvent event) {
             printLineToChatBox(username,messageBox.getText());
             serverOutput.println("MSG:*:"+messageBox.getText());
@@ -134,23 +150,31 @@ public class GUIClient {
     String username;
 
     class enterServerButtonListener implements ActionListener {
-        private final PrintWriter serverOutput;
-
-        enterServerButtonListener(PrintWriter serverOutput) {
-            this.serverOutput = serverOutput;
-        }
-
         public void actionPerformed(ActionEvent event) {
+            username = usernameChooser.getText();
+            String ip = ipAddress.getText();
+            int portNumber = Integer.parseInt(port.getText());
             username = usernameChooser.getText();
             if (username.length() < 1) {
                 System.out.println("No!");
             }
             else {
-                serverOutput.println("LOGIN:"+username);
-                preFrame.setVisible(false);
-                renderChatFrame();
+                try {
+                    connectToServer(ip,portNumber);
+                    serverOutput.println("LOGIN:"+username);
+                    preFrame.setVisible(false);
+                    renderChatFrame();
+                } catch (IOException e) {
+                    System.out.println("Couldn't connect to: "+ip+":"+portNumber);
+                }
             }
         }
 
+    }
+
+    private void connectToServer(String ip, int port) throws IOException {
+        serverSocket = new Socket(ip, port);
+        serverOutput = new PrintWriter(serverSocket.getOutputStream(), true);
+        serverInput = new Scanner(serverSocket.getInputStream());
     }
 }
